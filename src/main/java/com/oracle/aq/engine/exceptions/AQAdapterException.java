9a581027de1b3fb1e82262253b7bda625f68ec52
wickjason <wickjason2427@gmail.com>
2025-06-27 17:09:06 +0530

    Add files via upload

--- src/main/java/com/oracle/aq/engine/exceptions/AQAdapterException.java
+++ src/main/java/com/oracle/aq/engine/exceptions/AQAdapterException.java
@@ -1,1 +1,18 @@
+package com.oracle.aq.engine.exceptions;
 
+public class AQAdapterException extends Exception {
+
+	  private static final long serialVersionUID = -1246355274460089403L;
+
+	  public AQAdapterException(final String message) {
+	    super(message);
+	  }
+
+	  public AQAdapterException(final Throwable cause) {
+	    super(cause);
+	  }
+
+	  public AQAdapterException(final String message, final Throwable cause) {
+	    super(message, cause);
+	  }
+	}
