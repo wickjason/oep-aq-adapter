acb10307f1760870bf3345ec494fc752aaeba68b
wickjason <wickjason2427@gmail.com>
2025-06-27 17:00:57 +0530

    Create AqAdapterApplication.java

---
+++ src/main/java/com/oracle/aq/engine/AqAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.aq.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqAdapterApplication.class, args);
+	}
+
+}
